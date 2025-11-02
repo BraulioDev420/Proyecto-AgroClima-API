29a1ae06997d7a7f2a074121253168077e565117
Braulio Castro <braulio.castro@unisimon.edu.co>
2025-11-02 15:08:39 -0500

    Actualizar conexion.js

--- config/conexion.js
+++ config/conexion.js
@@ -1,15 +1,13 @@
 //--- Conexion a al BD MySQL
 //se carga el paquete o llama a la dependencia​
 const mysql = require('mysql');
-const conexion = mysql.createConnection(
-    {
-        host:'localhost',
-        user:'root',
-        password:'',
-        database:'agroclima13'
-    }
-
-);
+const conexion = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 // se abre oa conexion a la BD​
 conexion.connect(
     err=>{
